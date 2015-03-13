0d7f1ddf3b9c4f1fcf106ed0d0df30d4828bbe14
CorentinDelorme <delormecorentin@gmail.com>
2015-03-13 15:33:49 +0100

    Modification de la vue partie et des require dans le BinHero.rb. Ajout des images.

--- App/BinHero.rb
+++ App/BinHero.rb
@@ -1,19 +1,21 @@
 require 'gtk3'
 include Gtk
 
-require_relative './Controleur/ControleurMenuPrincipal'
+require_relative './Controleur/ControleurDemarrage'
+require_relative './Vue/VueMenuPrincipal'
+require_relative './Vue/VuePartie'
+require_relative './Modele/Grille'
+require_relative './Modele/Tuile'
 
 class BinHero
 
-	@controleur
+    @controleur
 
-    attr_writer :controleur
-
-	def initialize
-		Gtk.init
-		@controleur = ControleurMenuPrincipal.new(self)
-		Gtk.main
-	end
+    def initialize
+        Gtk.init
+        @controleur = ControleurDemarrage.new(self)
+        Gtk.main
+    end
 end
 
 binhero = BinHero.new()
