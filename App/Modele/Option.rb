12e3b52a298dcc2f8b02a75b84c349084f3bdb74
heraknos <heraknoshsdj@hotmail.fr>
2015-03-24 16:23:35 +0100

    Modif tuiles 0-1 en 1-2 (0 etant vide)

--- App/Modele/Option.rb
+++ App/Modele/Option.rb
@@ -1,13 +1,13 @@
 class Option
-    @tuile0 #chemin du fichier en string
-    @tuile1
+    @tuile1 #chemin du fichier en string
+    @tuile2
     @langue
     @tuileRouge='Vue/img/CaseRouge32.png'
     @tuileBleue='Vue/img/CaseBleue32.png'
     @tuileVerte='Vue/img/CaseVerte32.png'
     @tuileJaune='Vue/img/CaseJaune32.png'
     
-    attr_accessor :tuile0, :tuile1, :langue
+    attr_accessor :tuile1, :tuile2, :langue
     
     private_class_method :new
     
@@ -17,24 +17,24 @@ class Option
     end
     
     def initialize()
-        @tuile0=@tuileBleue
-        @tuile1=@tuileRouge
+        @tuile1=@tuileBleue
+        @tuile2=@tuileRouge
         #@langue='Vue/img/langue.lang'
     end
     
-    def changerTuile0(couleur)# couleur est une tuile choisie par clic
-        if(couleur==@tuile1)
-            #gerer l'affichage de l'erreur de meme couleur
-        else
-            @tuile0=couleur
-        end
-    end
-    
     def changerTuile1(couleur)# couleur est une tuile choisie par clic
-        if(couleur==@tuile0)
+        if(couleur==@tuile2)
             #gerer l'affichage de l'erreur de meme couleur
         else
             @tuile1=couleur
+        end
+    end
+    
+    def changerTuile2(couleur)# couleur est une tuile choisie par clic
+        if(couleur==@tuile1)
+            #gerer l'affichage de l'erreur de meme couleur
+        else
+            @tuile2=couleur
         end
     end
 end
