464413280e9b151f60f66a8b2104a8f505e46118
Kévin Demaret <demaret.kevin@orange.fr>
2015-03-24 14:53:09 +0100

    Ajout méthodes gestionnaire utilisateurs

--- App/Modele/Utilisateur.rb
+++ App/Modele/Utilisateur.rb
@@ -1,7 +1,8 @@
 class Utilisateur
     attr_accessor :id, :uuid, :nom, :motDePasse, :dateInscription, :dateDerniereSync, :type, :statistique, :option
 
-    #private_class_method :new
+    # private_class_method :initialize
+	
     # Méthode de création de création d'une tuile
     def Utilisateur.creer(id, uuid, nom, motDePasse, dateInscription, dateDerniereSync, type, statistique, option)
         new(id, uuid, nom, motDePasse, dateInscription, dateDerniereSync, type, statistique, option)
--- App/Modele/test.rb
+++ App/Modele/test.rb
@@ -1,10 +1,23 @@
 # Dépendances
 require "sqlite3"
 require "./GestionnaireUtilisateur.rb"
+require "./Utilisateur.rb"
 
-gu = GestionnaireUtilisateur.new()
+gestionnaireUtilisateur = GestionnaireUtilisateur.new()
 
-rs = gu.count()
 
-row = rs.next
-puts row.join "\s"
+resultat = gestionnaireUtilisateur.count()
+puts "Il y a #{resultat} utilisateur(s)"
+
+
+client = gestionnaireUtilisateur.getForAuthentication('toto0', 'azerty')
+if ( client == nil )
+	puts "Les identifiants ne sont pas correctes"
+else
+	puts "Bonjour #{ client.motDePasse }"
+end
+
+
+# puts "Ces derniers sont :"
+# resultat = gestionnaireUtilisateur.getAll(0, 10)
+# puts resultat
