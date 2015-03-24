239e0461554a08e96150c54be55d23f06e9e0c0a
Kévin Demaret <demaret.kevin@orange.fr>
2015-03-24 15:05:32 +0100

    Organisation scripts de tests pour les gestionnaires

--- App/Modele/Utilisateur.rb
+++ App/Modele/Utilisateur.rb
@@ -1,14 +1,13 @@
 class Utilisateur
     attr_accessor :id, :uuid, :nom, :motDePasse, :dateInscription, :dateDerniereSync, :type, :statistique, :option
-
-    # private_class_method :initialize
 	
     # Méthode de création de création d'une tuile
     def Utilisateur.creer(id, uuid, nom, motDePasse, dateInscription, dateDerniereSync, type, statistique, option)
         new(id, uuid, nom, motDePasse, dateInscription, dateDerniereSync, type, statistique, option)
     end
-
+	
+	private_class_method :new
     def initialize(id, uuid, nom, motDePasse, dateInscription, dateDerniereSync, type, statistique, option)
-	@id,@uuid,@nom,@motDePasse,@dateInscription,@dateDerniereSync,@type,@statistique,@option = id,uuid,nom,motDePasse,dateInscription,dateDerniereSync, type, statistique, option
+		@id,@uuid,@nom,@motDePasse,@dateInscription,@dateDerniereSync,@type,@statistique,@option = id,uuid,nom,motDePasse,dateInscription,dateDerniereSync, type, statistique, option
     end
 end
