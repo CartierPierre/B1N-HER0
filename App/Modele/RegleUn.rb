1f755b14d223d8f80b84e5f48f79be26f0f5b9ee
tianqiwei <weitianqiccc3@gmail.com>
2015-04-03 14:24:06 +0200

    Update RegleUn.rb

--- App/Modele/RegleUn.rb
+++ App/Modele/RegleUn.rb
@@ -7,61 +7,61 @@ class RegleUn
 		
 	private_class_method :new
     
-    def RegleUn.creer()
-        new()
-    end
+    	def RegleUn.creer()
+        	new()
+    	end
     	
-    def initialize()
-    	new()
-    end
+    	def initialize()
+    		new()
+	end
 	
 	def appliquer(grille)
 		@grille = grille
-    	@ligne = grille.ligne()
-    	@colonne = grille.colonne()
+    		@ligne = grille.ligne()
+    		@colonne = grille.colonne()
     		
-    	i = 0
-    	j = 0
-    	nbCouleurUn = 0
-    	nbCouleurDeux = 0
-    	while i < @ligne do
-    		while j < @colonne do
-    			if grille.getTuile(i,j).etat() == 1 then
-    				nbCouleurUn += 1
-    				
-    			elsif grille.getTuile(i,j).etat() == 2 then
-    				nbCouleurDeux += 1
-    			else
-    			end
-    			j += 1
-    		end
-    		if nbCouleurUn != nbCouleurDeux then
-    			return false
-    		end
-    		i += 1
-    	end
-    		
-    	i = 0
-    	j = 0
-    	nbCouleurUn = 0
-    	nbCouleurDeux = 0
-    	while j < @colonne do
+    		i = 0
+    		j = 0
+    		nbCouleurUn = 0
+    		nbCouleurDeux = 0
     		while i < @ligne do
-    			if grille.getTuile(i,j).etat() == 1 then
-    				nbCouleurUn += 1;
+    			while j < @colonne do
+    				if grille.getTuile(i,j).etat() == 1 then
+    					nbCouleurUn += 1
     				
-    			elsif grille.getTuile(i,j).etat() == 2 then
-    				nbCouleurDeux += 1;
-    				
-    			else
+    				elsif grille.getTuile(i,j).etat() == 2 then
+    					nbCouleurDeux += 1
+    				else
+    				end
+    				j += 1
+    			end
+    			if nbCouleurUn != nbCouleurDeux then
+    				return false
     			end
     			i += 1
     		end
-    		if nbCouleurUn != nbCouleurDeux then
-    			return false
+    		
+    		i = 0
+    		j = 0
+    		nbCouleurUn = 0
+    		nbCouleurDeux = 0
+    		while j < @colonne do
+    			while i < @ligne do
+    				if grille.getTuile(i,j).etat() == 1 then
+    					nbCouleurUn += 1;
+    				
+    				elsif grille.getTuile(i,j).etat() == 2 then
+    					nbCouleurDeux += 1;
+    				
+    				else
+    				end
+    				i += 1
+    			end
+    			if nbCouleurUn != nbCouleurDeux then
+    				return false
+    			end
+    			j += 1
     		end
-    		j += 1
+    		return true
     	end
-    	return true
-    end
 end
