32e412835766f63d2ecb8ba3f729e6e346fdcd35
Wei Tianqi <spi3041@ic2s120-08.univ-lemans.fr>
2015-04-03 16:35:58 +0200

    Ajout regle3

---
+++ App/Modele/RegleTrois.rb
@@ -0,0 +1,26 @@
+#Règle 3: 2 lignes ou 2 colonnes ne peuvent être identiques.
+#pas fini
+
+load 'Grille.rb'
+
+class RegleTrois
+		
+	private_class_method :new
+    	
+    def RegleTrois.creer()
+        new()
+    end
+    	
+    def initialize()
+    	new()
+    end
+    
+    def appliquer(partie)
+		
+		
+		    		
+    
+    				
+    			
+    			
+    	
