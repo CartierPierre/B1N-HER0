43a033ea23af717e6eea822b96eff72c1fde188c
tianqiwei <weitianqiccc3@gmail.com>
2015-04-03 13:47:24 +0200

    Update RegleDeux.rb

--- App/Modele/RegleDeux.rb
+++ App/Modele/RegleDeux.rb
@@ -4,9 +4,9 @@ load 'Grille.rb'
 
 class RegleDeux
 		
-	attr_reader:ligne, :colonne, :grille
+    attr_reader:ligne, :colonne, :grille
 		
-	private_class_method :new
+    private_class_method :new
 	
     def RegleDeux.creer()
         new()
