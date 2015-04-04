f5240bda8de59c90f2cc0213d214eb10f58a6efc
Kévin Demaret <demaret.kevin@orange.fr>
2015-04-04 20:10:53 +0200

    Ajout Super Script de test

---
+++ App/Test/TestTousGestionnaires.rb
@@ -0,0 +1,22 @@
+##
+# Script de test pour tous les gestionnaires
+#
+# Version 1
+#
+
+# Dépendances
+require_relative "../requireTout.rb"
+
+require "./TestGestionnaireUtilisateur.rb"
+
+puts
+
+require "./TestGestionnaireNiveau.rb"
+
+puts
+
+require "./TestGestionnaireScore.rb"
+
+puts
+
+require "./TestGestionnaireSauvegarde.rb"
