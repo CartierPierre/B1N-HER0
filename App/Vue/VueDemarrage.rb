e1521324bea1e7bd76955e89331ab50dd01fc9dc
Strauss72 <loicguen@gmail.com>
2015-04-07 11:18:01 +0200

    test couleur bouton

--- App/Vue/VueDemarrage.rb
+++ App/Vue/VueDemarrage.rb
@@ -1,5 +1,7 @@
 require_relative 'Vue'
 
+
+
 class VueDemarrage < Vue
 
     def initialize(modele,titre,controleur)
@@ -12,6 +14,13 @@ class VueDemarrage < Vue
         buttonInscription = Button.new(:label => "Inscription")
         buttonJeu = Button.new(:label => "Test jeu")
 
+        color=Gdk::RGBA::new(1.0,0.3,0.3,1.0)
+        buttonConnexion.override_background_color(0,color)
+
+        colorBlue = Gdk::Color.parse("#6E6F71")
+
+        colorRed = Gdk::Color.parse("#75CAF4")
+
         pixbufHero = Gdk::Pixbuf.new(:file => './Ressources/Hero.png', :width => 400, :height => 400)
         pixbufTitre = Gdk::Pixbuf.new(:file => './Ressources/Titre.png')
 
@@ -22,10 +31,12 @@ class VueDemarrage < Vue
         vbox.add(buttonConnexion)
         vbox.add(buttonInscription)
         vbox.add(buttonJeu)
+        vbox.set_homogeneous(true);
 
         hbox.add(imgHero)
         hbox.add(vbox)
 
+        @cadre.override_background_color(:normal,Gdk::RGBA::new(1, 1, 1, 1))
         @cadre.add(hbox)
 
         buttonConnexion.signal_connect('clicked')  {onBtnConnexionClicked}
