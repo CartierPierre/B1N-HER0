270467e4049643b583ff0efe1169d6809af78f44
Wei Tianqi <spi3041@ic2s130-06.univ-lemans.fr>
2015-04-07 09:51:15 +0200

    Modification de regle 3

--- App/Modele/RegleTrois.rb
+++ App/Modele/RegleTrois.rb
@@ -2,6 +2,7 @@
 #pas fini
 
 load 'Grille.rb'
+load "Partie.rb"
 
 class RegleTrois
 		
@@ -15,12 +16,32 @@ class RegleTrois
     	new()
     end
     
-    def appliquer(partie)
-		
-		
-		    		
-    
-    				
+    def RegleTrois.appliquer(partie)
+    	#Pour la ligne
+    	0.upto(Math.sqrt(partie.grille().taille() - 1) do |x|
+    		tab = getLigne(x)
+			x.upto(Math.sqrt(partie.grille().taille() - 1) do |z|
+				if !(tab & getLigne(z)).empty? #si il y a intersection entre deux ligne 
+					if((tab & getLigne(z)).size == partie.grille().taille()) #Si la taille d'intersection est égale à la taille de ligne
+		    			return false
+    				end
+    			end
+    		end
+    	end
+    	#Pour la colonne
+    	0.upto(Math.sqrt(partie.grille().taille() - 1) do |y|
+    		tab = getLigne(y)
+			x.upto(Math.sqrt(partie.grille().taille() - 1) do |i|	
+    			if !(tab & getColonne(i)).empty? #si il y a intersection entre deux ligne 
+					if((tab & getColonne(z)).size == partie.grille().taille()) #Si la taille d'intersection est égale à la taille de ligne
+		    			return false
+    				end
+    			end
+    		end
+    	end
+    	return true
+    end
+end				
     			
     			
     	
