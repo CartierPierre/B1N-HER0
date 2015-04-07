92e4be064d213a5a2bcb38dac699c3226d29f4a7
Wei Tianqi <spi3041@ic2s130-06.univ-lemans.fr>
2015-04-07 12:01:45 +0200

    Update Regle3

--- App/Modele/RegleTrois.rb
+++ App/Modele/RegleTrois.rb
@@ -19,7 +19,7 @@ class RegleTrois
     	#Pour chaque ligne
     	0.upto Math.sqrt(partie.grille().taille() - 1) do |x|
 			x.upto Math.sqrt(partie.grille().taille() - 1) do |z|
-				if !(getLigne(x) & getLigne(z).empty?) && (getLigne(x) & getLigne(z)).size == partie.grille().taille()
+				if !((partie.grille.getLigne(x) & partie.grille.getLigne(z)).empty?) && (partie.grille.getLigne(x) & partie.grille.getLigne(z)).size == partie.grille().taille()
 					#si il y a intersection entre deux ligne et la taille d'intersection est égale à la taille de ligne
 		    			return false
     			end
@@ -27,9 +27,8 @@ class RegleTrois
     	end
     	#Pour chaque colonne
     	0.upto Math.sqrt(partie.grille().taille() - 1) do |y|
-    		tab = getColonne(y)
 			x.upto Math.sqrt(partie.grille().taille() - 1) do |z|
-				if !(getColonne(y) & getColonne(z).empty?) && (getColonne(y) & getColonne(z)).size == partie.grille().taille()
+				if !((partie.grille.getColonne(y) & partie.grille.getColonne(z)).empty?) && (partie.grille.getColonne(y) & partie.grille.getColonne(z)).size == partie.grille().taille()
 				#si il y a intersection entre deux colonne et la taille d'intersection est égale à la taille de colonne	
 		    			return false
     			end
