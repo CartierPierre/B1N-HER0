af9ee1bfb35ef500fe94aede978031b28e947d16
Wei Tianqi <spi3041@ic2s130-06.univ-lemans.fr>
2015-04-07 12:01:08 +0200

    Update Regle2

--- App/Modele/RegleDeux.rb
+++ App/Modele/RegleDeux.rb
@@ -2,6 +2,7 @@
 
 load 'Grille.rb'
 load "Partie.rb"
+load "Etat.rb"
 
 class RegleDeux
 		
@@ -18,24 +19,20 @@ class RegleDeux
     def RegleDeux.appliquer(partie)
 
 	0.upto Math.sqrt(partie.grille().taille() - 1) do |x|
-		0.upto Math.sqrt(partie.grille().taille() - 2) do |y|
- 			if partie.grille().getTuile(x,y).etat() == 1 && partie.grille().getTuile(x,y+1).etat() == 1 && partie.grille().getTuile(x,y+2).etat() == 1 then
+		0.upto Math.sqrt(partie.grille().taille() - 2) do |y| 
+ 			if Etat.egale?(partie.grille().getTuile(x,y).etat(),partie.grille().getTuile(x,y+1).etat()) && Etat.egale?(partie.grille().getTuile(x,y+1).etat(),partie.grille().getTuile(x,y+2).etat()) then
  					#Meme si plus que trois, il retourne faux 
  					return false
- 					
-    			elsif partie.grille().getTuile(x,y).etat() == 2 && partie.grille().getTuile(x,y+1).etat() == 2 && partie.grille().getTuile(x,y+2).etat() == 2 then
- 					return false
-    			end
     		end
     	end
 
-		0.upto Math.sqrt(partie.grille().taille() - 1) do |y|
-			0.upto Math.sqrt(partie.grille().taille() - 2) do |x|
-    			if partie.grille().getTuile(x,y).etat() == 1 && partie.grille().getTuile(x+1,y).etat() == 1 && partie.grille().getTuile(x+2,y).etat() == 1 then
+	0.upto Math.sqrt(partie.grille().taille() - 1) do |y|
+		0.upto Math.sqrt(partie.grille().taille() - 2) do |x|
+    		if partie.grille().getTuile(x,y).etat() == Etat.etat_1 && partie.grille().getTuile(x+1,y).etat() == Etat.etat_1 && partie.grille().getTuile(x+2,y).etat() == Etat.etat_1 then
  					#Meme si plus que trois, il retourne faux 
  					return false
  					
-    			elsif partie.grille().getTuile(x,y).etat() == 2 && partie.grille().getTuile(x+1,y).etat() == 2 && partie.grille().getTuile(x+2,y).etat() == 2 then
+    		elsif partie.grille().getTuile(x,y).etat() == Etat.etat_2 && partie.grille().getTuile(x+1,y).etat() == Etat.etat_2 && partie.grille().getTuile(x+2,y).etat() == Etat.etat_2 then
  					return false
     			end			
     		end
