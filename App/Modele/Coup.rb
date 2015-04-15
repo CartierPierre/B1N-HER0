f07b1a8970324ac501f65108fd25da9394563220
Amaury Savarre <amaury.savarre@gmail.com>
2015-04-15 18:21:07 +0200

    Ajout de commentaire pour la doc

--- App/Modele/Coup.rb
+++ App/Modele/Coup.rb
@@ -1,7 +1,17 @@
+##
+# La classe Coup permet de créer et utiliser des coups.
+
 class Coup
     attr_reader :x, :y
     attr_accessor :etat
 
+    ##
+    # Méthode de création d'un Coup.
+    #
+    # Paramétres::
+    #   * _x_ - La coordonnée x du Coup.
+    #   * _y_ - La coordonnée y du Coup.
+    #   * _etat_ - L'état de la Tuile avant le Coup.
     private_class_method :new
     def Coup.creer(x, y, etat)
         new(x, y, etat)
