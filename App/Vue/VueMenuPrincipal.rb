9bda2125e25dddb7e8aefcd8664d65646684d775
CorentinDelorme <Corentin.Delorme.Etu@univ-lemans.fr>
2015-04-25 15:07:31 +0200

    Ajout des commentaires pour la VueCredits et VueMenuPrincipal.

--- App/Vue/VueMenuPrincipal.rb
+++ App/Vue/VueMenuPrincipal.rb
@@ -1,5 +1,7 @@
 class VueMenuPrincipal < Vue
 
+    ### Attributs d'instances
+
 	@boutonNouvellePartie
     @boutonChargerPartie
 	@boutonClassement
@@ -8,11 +10,21 @@ class VueMenuPrincipal < Vue
     @boutonCredits
 	@boutonQuitter
 
+    ##
+    # Méthode de création de la vue menu principal qui permet de sélectionner 
+    # les différents éléments et fonctionnalités du jeu
+    #
+    # Paramètres::
+    #   * _modele_ - Modèle associé
+    #   * _titre_ - Titre de la fenetre
+    #   * _controleur_ - Controleur associé 
+    #
 	def initialize(modele,titre,controleur)
 		super(modele,titre,controleur)
 
 		vboxPrincipale = Box.new(:vertical, 20)
 
+        # Création des boutons
 		@boutonNouvellePartie = Button.new(:label => @controleur.getLangue[:nouvellePartie])
         @boutonNouvellePartie.set_size_request(100,35)
         @boutonChargerPartie = Button.new(:label => @controleur.getLangue[:chargerPartie])
@@ -28,6 +40,7 @@ class VueMenuPrincipal < Vue
 		@boutonQuitter = Button.new(:label => @controleur.getLangue[:quitter])
         @boutonQuitter.set_size_request(100,35)
 
+        # Ajout des boutons dans la vbox principale
         vboxPrincipale.pack_start(Alignment.new(0, 0, 0, 0), :expand => true)
         creerAlignBouton(vboxPrincipale,@boutonNouvellePartie)
         creerAlignBouton(vboxPrincipale,@boutonChargerPartie)
@@ -40,10 +53,11 @@ class VueMenuPrincipal < Vue
 
         @cadre.add(vboxPrincipale)
 
+        # Signaux des boutons
         @boutonNouvellePartie.signal_connect('clicked') { onBtnNouvellePartieClicked }
         @boutonChargerPartie.signal_connect('clicked') { onBtnChargerPartieClicked }
-        @boutonOptions.signal_connect('clicked') { onBtnOptionsClicked }
         @boutonClassement.signal_connect('clicked') { onBtnClassementClicked }
+        @boutonOptions.signal_connect('clicked') { onBtnOptionsClicked }        
         @boutonProfil.signal_connect('clicked') { onBtnProfilClicked }        
         @boutonCredits.signal_connect('clicked') { onBtnCreditsClicked }
         @boutonQuitter.signal_connect('clicked') { Gtk.main_quit }
@@ -51,34 +65,58 @@ class VueMenuPrincipal < Vue
         self.actualiser()
 	end
 	
+    ##
+    # Listener sur le bouton nouvelle partie
+    # Ferme le cadre et ouvre la vue qui permet de lancer une nouvelle partie
+    #
 	def onBtnNouvellePartieClicked
         fermerCadre()
         @controleur.nouvellePartie()
 	end
 
+    ##
+    # Listener sur le bouton charger partie
+    # Ferme le cadre et ouvre la vue qui permet de charger une partie précédente
+    #
     def onBtnChargerPartieClicked
         fermerCadre()
         @controleur.chargerPartie()
     end
 
+    ##
+    # Listener sur le bouton classement
+    # Ferme le cadre et ouvre la vue qui permet de voir le classement du jeu
+    #
+    def onBtnClassementClicked
+        fermerCadre()
+        @controleur.classement()
+    end
+
+    ##
+    # Listener sur le bouton options
+    # Ferme le cadre et ouvre la vue qui permet de configurer les options du jeu
+    #
 	def onBtnOptionsClicked
         fermerCadre()
 		@controleur.options()
 	end
 
+    ##
+    # Listener sur le bouton profil
+    # Ferme le cadre et ouvre la vue qui permet de voir le profil de l'utilisateur connecté
+    #
+    def onBtnProfilClicked
+        fermerCadre()
+        @controleur.profil()
+    end
+
+    ##
+    # Listener sur le bouton crédits
+    # Ferme le cadre et ouvre la vue qui affiche les crédits du jeu
+    #
     def onBtnCreditsClicked
         fermerCadre()
         @controleur.credits()
-    end
-
-	def onBtnProfilClicked
-        fermerCadre()
-		@controleur.profil()
-	end
-
-    def onBtnClassementClicked
-        fermerCadre()
-        @controleur.classement()
     end
 
 end
